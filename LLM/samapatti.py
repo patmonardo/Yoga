f305c91ef91e6f0a6f5992239782693ba2c5d87b
Pat Monardo <patmonardo@gmail.com>
2023-08-25 13:57:38 -0700

    Dharma Greater GPT

--- LLM/samapatti.py
+++
@@ -1,8 +0,0 @@
-YS III.41.
-kaya-akasa sambandha-samyama laghu-tula-samapatti cakasa-gamanam
-
-YS III.44
-bahir akalpita vrttir maha-videha tatah prakasavarana-ksayah
-
-III.48
-grahana-svarupasmitanvayarthavattva-samyamad indriya-jaya
